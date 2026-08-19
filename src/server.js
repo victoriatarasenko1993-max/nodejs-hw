a69618177f3116fef5fad1305309733214e34b11
Вікторія <victoriatarasenko1993@gmail.com>
2026-08-20 01:07:09 +0300

    Refactor server.js to use environment variables and add routes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,35 @@
+import 'dotenv/config';
 import express from 'express';
+import cors from 'cors';
+import pinoHttp from 'pino-http';
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.use(pinoHttp());
+app.use(cors());
+app.use(express.json());
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  res.status(200).json({
+    message: `Retrieved note with ID: ${req.params.noteId}`,
+  });
+});
+
+app.get('/test-error', () => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((error, req, res, next) => {
+  res.status(500).json({ message: error.message });
 });
 
 app.listen(PORT, () => {
