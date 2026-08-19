24a8a2fb3d77143fc1356e16e1cab95d40a925f0
victoriatarasenko1993-max <victoriatarasenko1993@gmail.com>
2026-08-20 00:47:41 +0300

    hw1

--- src/server.js
+++ src/server.js
@@ -1,10 +1,59 @@
 import express from 'express';
+import cors from 'cors';
+import helmet from 'helmet';
+import pino from 'pino-http';
+import 'dotenv/config';
 
+const PORT = process.env.PORT ?? 3000;
 const app = express();
-const PORT = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Hello world!' });
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(
+  pino({
+    level: 'info',
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        colorize: true,
+        translateTime: 'HH:MM:ss',
+        ignore: 'pid,hostname',
+        messageFormat:
+          '{req.method} {req.url} {res.statusCode} - {responseTime}ms',
+        hideObject: true,
+      },
+    },
+  }),
+);
+
+app.get('/notes', (req, res) => {
+  res.status(200).json({ message: 'Retrieved all notes' });
+});
+
+app.get('/notes/:noteId', (req, res) => {
+  const noteId = req.params.noteId;
+  res.status(200).json({
+    message: `Retrieved note with ID: ${noteId}`,
+  });
+});
+
+app.get('/test-error', (req, res) => {
+  throw new Error('Simulated server error');
+});
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Route not found' });
+});
+
+app.use((err, req, res, next) => {
+  const isProd = process.env.NODE_ENV === 'production';
+
+  res.status(500).json({
+    message: isProd
+      ? 'Something went wrong. Please try again later.'
+      : err.message,
+  });
 });
 
 app.listen(PORT, () => {
